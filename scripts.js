72ceb57dc787d25614739ec25f42ab153e6da1d3
RishiBharadwaj31 <153712438+RishiBharadwaj31@users.noreply.github.com>
2023-12-17 17:16:41 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,16 @@
+// scripts.js
+
+// Example of JavaScript functionality (for demonstration purposes)
+// This script could handle adding items to the cart or any other functionalities
+
+// Example function to add a product to the cart
+function addToCart(productId) {
+    // Your logic to add the product to the cart
+    console.log(`Product with ID ${productId} added to cart`);
+}
+
+// Example function to handle checkout process
+function checkout() {
+    // Your logic to proceed with the checkout process
+    console.log('Checkout initiated');
+}
